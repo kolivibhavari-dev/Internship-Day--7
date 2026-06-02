3c9c00fdba59ca4af05f8ee1567875e4702afad0
kolivibhavari-dev <kolivibhavari@gmail.com>
2026-06-02 12:57:05 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,19 @@
+for(let i = 0; i<5; i++){
+    if(i%2 == 0){
+        console.log(i + "is even number");
+    }else{
+        console.log(i + "is odd number");
+    }
+
+
+}
+
+for(let i = 1; i<=10; i++){
+    if(i == 5){
+        console.log("skip");
+        continue;
+    }
+    console.log(i);
+}
+
+
