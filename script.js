a4beceb440eb78d5d9dbb3d0099384d82ba2906b
kolivibhavari-dev <kolivibhavari@gmail.com>
2026-06-03 08:32:25 +0530

    adding files

--- script.js
+++ script.js
@@ -1,19 +1,88 @@
-for(let i = 0; i<5; i++){
-    if(i%2 == 0){
-        console.log(i + "is even number");
-    }else{
-        console.log(i + "is odd number");
-    }
+let num = 10;
+
+if (num > 0) console.log("Positive");
 
 
+let number = 10;
+
+number > 0 ? console.log("Positive") : console.log("Negative");
+
+
+
+let marks = 75;
+
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 75) {
+    console.log("Grade B");
+} else if (marks >= 50) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
 }
 
-for(let i = 1; i<=10; i++){
-    if(i == 5){
-        console.log("skip");
-        continue;
-    }
+
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    case 3:
+        console.log("Wednesday");
+        break;
+
+    case 4:
+        console.log("Thursday");
+        break;
+
+    case 5:
+        console.log("Friday");
+        break;
+
+    default:
+        console.log("Invalid day");
+}
+
+
+let age = 18;
+
+let message = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+
+console.log(message);
+
+for (let i = 1; i <= 5; i++) {
     console.log(i);
 }
 
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+
+
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+
+let student = {
+    name: "Tejas Pawar",
+    age: 20
+};
+
+for (let key in student) {
+    console.log(key + ": " + student[key]);
+}
 
